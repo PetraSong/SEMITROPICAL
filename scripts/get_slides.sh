30e24900de133b805984d406f05c06ac70420943
Sander W. van der Laan <s.w.vanderlaan@gmail.com>
2023-09-05 12:50:26 -0400

    fix

--- scripts/get_slides.sh
+++ scripts/get_slides.sh
@@ -35,7 +35,7 @@ STAIN="GLYCC"
 
 for STUDYNUMBER in ${AE_list2461}; do
 
-    echo "> Copying slides for study ${STUDYNUMBER} and stain $S{TAIN"
+    echo "> Copying slides for study ${STUDYNUMBER} and stain ${STAIN}"
     cp -v ${BULKDIR}/${STAIN}/AE${STUDYNUMBER}* ${VIRTUALSLIDESDIR}/${STAIN}/
 
 done
